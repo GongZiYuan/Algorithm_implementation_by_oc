90fb5c8a3fa074633e1344816f1b9c690f53f5d9
zhaoyuan <zhaoyuan@etouch.cn>
2015-07-27 18:22:05 +0800

    create

---
+++ Algorithm_implementation_by_oc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Algorithm_implementation_by_oc
+//
+//  Created by 赵远 on 15/7/27.
+//  Copyright (c) 2015年 GongZiYuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Algorithm_implementation_by_oc/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Algorithm_implementation_by_oc
+//
+//  Created by 赵远 on 15/7/27.
+//  Copyright (c) 2015年 GongZiYuan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
